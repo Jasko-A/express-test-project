051e22a3985a997346d794de21d8ac1ba32179f6
Jasko-A <{jadzic@ucdavis.edu}>
2019-04-09 17:15:33 -0700

    modularized routes and learned to display static files

--- src/index.js
+++ src/index.js
@@ -8,15 +8,18 @@ console.log(process.env.MY_SECRET);
 
 //////////////////////////////////////////////////////////
 
+
+import routes from './routes';
 import models from './models';
 
 require('dotenv').config(); //allow to use .env files for secret values
-var cors = require('cors');
+// var cors = require('cors');
 var express = require('express');
-const uuidv4 = require('uuid/v4');
-//var bodyParser = require('body-parser') //so npm packages can be imported this way
-import bodyParser from 'body-parser';	  //or this way below !!!
+// const uuidv4 = require('uuid/v4');
+var bodyParser = require('body-parser'); //so npm packages can be imported this way
+//import bodyParser from 'body-parser';	  //or this way below !!!
 var app = express();
+var path = require('path');
 
 
 // let users = {
@@ -46,117 +49,135 @@ var app = express();
 //ALL the json above will now be modularized and put into the models directory
 //in the index.js file
 
-app.use(cors())
+//app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
-	req.context = {
-		models,
-		me: models.users[1],
-	};
-  
-  	next();
+  console.log("HERE");
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
 });
+
+app.use(express.static('public'));
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+
+
+
 
 app.get('/', (req,res)=>{
-	res.send('\nGET')
+  //console.log(__dirname); //__dirname gives current running directory
+	res.sendFile(path.join(__dirname + '/public/index.html'), (err) =>
+    {
+      if(err)
+        res.status(403).send("Sorry! You can't see that.");
+
+    });
 });
 
-// app.get('/user/:userID', (req,res) =>{
-// 	res.send('Get user ID: ' + req.params.userID);
+// // app.get('/user/:userID', (req,res) =>{
+// // 	res.send('Get user ID: ' + req.params.userID);
+// // });
+// /*
+// app.get('/users', (req, res) => {
+//   res.send(Object.values(users));
 // });
-/*
-app.get('/users', (req, res) => {
-  res.send(Object.values(users));
-});
-*/
-app.get('/users', (req, res) => {
-  res.send(Object.values(req.context.models.users));
-});
-/*
-app.get('/users/:userId', (req, res) => {
-  res.send(users[req.params.userId]);
-});
-*/
-app.get('/users/:userId', (req, res) => {
-  res.send(req.context.models.users[req.params.userId]);
-});
-/*
-app.get('/messages', (req, res) => {
-  return res.send(Object.values(messages));
-});
-*/
-app.get('/messages', (req, res) => {
-  return res.send(Object.values(req.context.models.messages));
-});
-/*
-app.get('/messages/:messageId', (req, res) => {
-  return res.send(messages[req.params.messageId]);
-});
-*/
-app.get('/messages/:messageId', (req, res) => {
-  return res.send(req.context.models.messages[req.params.messageId]);
-});
+// */
+// app.get('/users', (req, res) => {
+//   res.send(Object.values(req.context.models.users));
+// });
+// /*
+// app.get('/users/:userId', (req, res) => {
+//   res.send(users[req.params.userId]);
+// });
+// */
+// app.get('/users/:userId', (req, res) => {
+//   res.send(req.context.models.users[req.params.userId]);
+// });
+// /*
+// app.get('/messages', (req, res) => {
+//   return res.send(Object.values(messages));
+// });
+// */
+// app.get('/messages', (req, res) => {
+//   return res.send(Object.values(req.context.models.messages));
+// });
+// /*
+// app.get('/messages/:messageId', (req, res) => {
+//   return res.send(messages[req.params.messageId]);
+// });
+// */
+// app.get('/messages/:messageId', (req, res) => {
+//   return res.send(req.context.models.messages[req.params.messageId]);
+// });
 
-app.post('/', (req,res)=>{
-	res.send('\nPOST')
-});
-/*
-app.post('/messages', (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    text: req.body.text,
-    userId: req.me.id,
-  };
+// app.post('/', (req,res)=>{
+// 	res.send('\nPOST')
+// });
+// /*
+// app.post('/messages', (req, res) => {
+//   const id = uuidv4();
+//   const message = {
+//     id,
+//     text: req.body.text,
+//     userId: req.me.id,
+//   };
 
-  messages[id] = message;
+//   messages[id] = message;
 
-  return res.send(message);
-});
-*/
-app.post('/messages', (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    text: req.body.text,
-    userId: req.context.me.id,
-  };
+//   return res.send(message);
+// });
+// */
+// app.post('/messages', (req, res) => {
+//   const id = uuidv4();
+//   const message = {
+//     id,
+//     text: req.body.text,
+//     userId: req.context.me.id,
+//   };
 
-  req.context.models.messages[id] = message;
+//   req.context.models.messages[id] = message;
 
-  return res.send(message);
-});
+//   return res.send(message);
+// });
 
-app.put('/', (req,res)=>{
-	res.send('\nPUT')
-});
+// app.put('/', (req,res)=>{
+// 	res.send('\nPUT')
+// });
 
-app.delete('/', (req,res)=>{
-	res.send('\nDELETE')
-});
-/*
-app.delete('/messages/:messageId', (req, res) => {
-  const {
-    [req.params.messageId]: message,
-    ...otherMessages
-  } = messages;
+// app.delete('/', (req,res)=>{
+// 	res.send('\nDELETE')
+// });
+// /*
+// app.delete('/messages/:messageId', (req, res) => {
+//   const {
+//     [req.params.messageId]: message,
+//     ...otherMessages
+//   } = messages;
 
-  messages = otherMessages;
+//   messages = otherMessages;
 
-  return res.send(message);
-});
-*/
-app.delete('/messages/:messageId', (req, res) => {
-  const {
-    [req.params.messageId]: message,
-    ...otherMessages
-  } = req.context.models.messages;
+//   return res.send(message);
+// });
+// */
 
-  req.context.models.messages = otherMessages;
+// app.delete('/messages/:messageId', (req, res) => {
+//   const {
+//     [req.params.messageId]: message,
+//     ...otherMessages
+//   } = req.context.models.messages;
 
-  return res.send(message);
-});
+//   req.context.models.messages = otherMessages;
+
+//   return res.send(message);
+// });
+
+
 
 app.listen(process.env.SECRET_PORT, () =>
 	console.log("Example listening on port " + process.env.SECRET_PORT),
