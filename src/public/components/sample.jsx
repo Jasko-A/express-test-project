66f3a47a84b06f6d7c2081362ec960b5ad9b492b
Jasko-A <jasmin.adzic@comcast.net>
2019-04-15 20:59:59 -0700

    we good?

---
+++ src/public/components/sample.jsx
@@ -0,0 +1,14 @@
+const { Component } = React;
+const { render } = ReactDOM;
+
+class Sample extends Component {  
+  render() {
+    return (
+      <div className="container">
+        <h3>React Form</h3>
+      </div>
+    );
+  }
+}
+
+export default Sample;
